cbf9ffe465a49003c2d7c7e8d2a72c3fc059e3d5
Ronald <pietjebell31@hotmail.com>
2008-06-22 17:59:05 +0000

    Removed file/folder
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk@3133 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- impression/list.tag.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * $Id: list.tag.php
- * Module: XoopsTube
-*/
-
-include "header.php";
-include XOOPS_ROOT_PATH."/modules/tag/list.tag.php";
-?>
--- impression/view.tag.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * $Id: view.tag.php
- * Module: Impression
-*/
-include "header.php";
-include XOOPS_ROOT_PATH."/modules/tag/view.tag.php";
-?>
