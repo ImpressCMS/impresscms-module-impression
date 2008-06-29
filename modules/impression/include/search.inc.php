3f65b9255ff124dd0656694e8f75d63d973cbcc9
Ronald <pietjebell31@hotmail.com>
2008-06-29 18:55:07 +0000

    Updated for renaming of module folder
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk@3378 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/impression/include/search.inc.php
+++ modules/impression/include/search.inc.php
@@ -4,24 +4,19 @@
  * Module: Impression
  */
 
-function impressioncheckSearchgroups( $cid = 0, $permType = 'ImpressionCatPerm', $redirect = false )
-{
+function impressioncheckSearchgroups( $cid = 0, $permType = 'ImpressionCatPerm', $redirect = false ) {
     global $xoopsUser;
-
+    $mydirname = basename( dirname( dirname( __FILE__ ) ) );
     $groups = is_object( $xoopsUser ) ? $xoopsUser -> getGroups() : XOOPS_GROUP_ANONYMOUS;
     $gperm_handler = &xoops_gethandler( 'groupperm' );
 
     $module_handler = &xoops_gethandler( 'module' );
-    $module = &$module_handler -> getByDirname( 'impression' );
+    $module = &$module_handler -> getByDirname( $mydirname );
 
-    if ( !$gperm_handler -> checkRight( $permType, $cid, $groups, $module -> getVar( 'mid' ) ) )
-    {
-        if ( $redirect == false )
-        {
+    if ( !$gperm_handler -> checkRight( $permType, $cid, $groups, $module -> getVar( 'mid' ) ) ) {
+        if ( $redirect == false ) {
             return false;
-        } 
-        else
-        {
+        } else {
             redirect_header( 'index.php', 3, _NOPERM );
             exit();
         } 
@@ -30,34 +25,27 @@ function impressioncheckSearchgroups( $cid = 0, $permType = 'ImpressionCatPerm',
     return true;
 } 
 
-function impression_search( $queryarray, $andor, $limit, $offset, $userid )
-{
+function impression_search( $queryarray, $andor, $limit, $offset, $userid ) {
     global $xoopsDB, $xoopsUser;
 
-//    $queryarray = strtolower($queryarray);
     $sql = "SELECT cid, pid FROM " . $xoopsDB -> prefix( 'impression_cat' );
     $result = $xoopsDB -> query( $sql );
-    while ( $_search_group_check = $xoopsDB -> fetchArray( $result ) )
-    {
+    while ( $_search_group_check = $xoopsDB -> fetchArray( $result ) ) {
         $_search_check_array[$_search_group_check['cid']] = $_search_group_check;
     } 
 
     $sql = "SELECT aid, cid, title, submitter, published, introtext, description FROM " . $xoopsDB -> prefix( 'impression_articles' );
-    $sql .= " WHERE published > 0 AND published <= " . time()
-     . " AND offline = 0 AND cid > 0";
+    $sql .= " WHERE published > 0 AND published <= " . time() . " AND offline = 0 AND cid > 0";
 
-    if ( $userid != 0 )
-    {
+    if ( $userid != 0 ) {
         $sql .= " AND submitter=" . $userid . " ";
     } 
 
     // because count() returns 1 even if a supplied variable
     // is not an array, we must check if $querryarray is really an array
-    if ( is_array( $queryarray ) && $count = count( $queryarray ) )
-    {
+    if ( is_array( $queryarray ) && $count = count( $queryarray ) ) {
         $sql .= " AND ((title LIKE LOWER('%$queryarray[0]%') OR LOWER(description) LIKE LOWER('%$queryarray[0]%'))";
-        for( $i = 1;$i < $count;$i++ )
-        {
+        for( $i = 1;$i < $count;$i++ ) {
             $sql .= " $andor ";
             $sql .= "(title LIKE LOWER('%$queryarray[$i]%') OR LOWER(description) LIKE LOWER('%$queryarray[$i]%'))";
         } 
@@ -68,10 +56,8 @@ function impression_search( $queryarray, $andor, $limit, $offset, $userid )
     $ret = array();
     $i = 0;
 
-    while ( $myrow = $xoopsDB -> fetchArray( $result ) )
-    {
-        if ( false == impressioncheckSearchgroups( $myrow['cid'] ) )
-        {
+    while ( $myrow = $xoopsDB -> fetchArray( $result ) ) {
+        if ( false == impressioncheckSearchgroups( $myrow['cid'] ) ) {
             continue;
         } 
         $ret[$i]['image'] = "images/size2.gif";
