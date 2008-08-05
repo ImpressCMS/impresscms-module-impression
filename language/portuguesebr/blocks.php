0af28a27f57e485a7d81732cc8e65798d92eec36
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-08-05 10:08:30 +0000

    [modules] - [Impression] - Started translation...folders and files...
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk/modules/impression@4041 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/portuguesebr/blocks.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * $Id: blocks.php
+ * Module: Impression
+ * Language: english
+ * Author: McDonald
+ */
+ 
+// Blocks
+define( '_MB_IMPRESSION_DISP', '<b>Display: </b>' );
+define( '_MB_IMPRESSION_FILES', 'Articles' );
+define( '_MB_IMPRESSION_CHARS', '<b>Length of the title: </b>' );
+define( '_MB_IMPRESSION_LENGTH', ' characters' );
+define( '_MB_IMPRESSION_EDIT', 'Edit article' );
+define( '_MB_IMPRESSION_DELETE', 'Delete article' );
+define( '_MB_IMPRESSION_APPROVE', 'Approve' );
+define( '_MB_IMPRESSION_CATEGORY', 'Category' );
+define( '_MB_IMPRESSION_ARTICLE', 'Article title' );
+define( '_MB_IMPRESSION_DATE', 'Date' );
+define( '_MB_IMPRESSION_HEADLINES', 'headlines' );
+define( '_MB_IMPRESSION_ADMINSECTION', 'Administrative section' );
+define( '_MB_IMPRESSION_DATEFORMAT', '<b>Date format: </b>' );
+define( '_MB_IMPRESSION_DATEFORMATMANUAL', 'See <a href="http://jp.php.net/manual/en/function.date.php" target="_blank">PHP manual</a>' );
+define( '_MB_IMPRESSION_SHOWRSSICONS', '<b>Display RSS icons: </b>' );
+define( '_MB_IMPRESSION_SHOWRSSICONS_DSC', '(sub-feed needs to be activated in RSSFit)' );
+define( '_MB_IMPRESSION_ADDGOOGLE', 'Google Reader or Homepage' );
+define( '_MB_IMPRESSION_ADDMYYAHOO', 'Add to My Yahoo!' );
+define( '_MB_IMPRESSION_ADDNEWSGATOR', 'Subscribe in NewsGator Online' );
+define( '_MB_IMPRESSION_ADDAOL', 'Add to My AOL' );
+define( '_MB_IMPRESSION_ADDMSLIVE', 'Add to Windows Live' );
+define( '_MB_IMPRESSION_READMORE', 'Read more' );
+define( "_MB_IMPRESSION_PUBLISHEDON", "Published on " );
+define( "_MB_IMPRESSION_BY", " by " );
+define( "_MB_IMPRESSION_ARTICLEHITS", "(%s reads)" );
+?>
