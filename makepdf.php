588a97ec4c9592b3ed0f73b94656025788d5d15d
Ronald <pietjebell31@hotmail.com>
2008-08-20 19:44:19 +0000

    Set font-size and cleaned up code
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk/modules/impression@4476 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- makepdf.php
+++ makepdf.php
@@ -11,18 +11,18 @@ if (!defined('ICMS_ROOT_PATH')) {
 }
 
 function strip_p_tag($text) {
-         $search = array(
+    $search = array(
          "'<p>'si",
          "'</p>'si",
 	);
 
-	 $replace = array(
+	$replace = array(
          "",
          "<br /><br />",
 	);
 
 	$text = preg_replace($search, $replace, $text);
-        return $text;
+    return $text;
 }
 
 $mydirname = basename( dirname( __FILE__ ) );
@@ -57,17 +57,17 @@ require_once ICMS_PDF_LIB_PATH.'/tcpdf.php';
 
 $filename = XOOPS_ROOT_PATH.'/modules/' . $mydirname . '/' . $xoopsConfig['language'] . '/main.php';
 
-if (file_exists( $filename)) {
+if ( file_exists( $filename ) ) {
   include_once $filename;
 } else {
-  include_once XOOPS_ROOT_PATH.'/modules/' . $mydirname . '/language/english/main.php';
+  include_once XOOPS_ROOT_PATH . '/modules/' . $mydirname . '/language/english/main.php';
 }
 
-$filename = ICMS_PDF_LIB_PATH.'/config/lang/'._LANGCODE.'.php';
-if(file_exists($filename)) {
+$filename = ICMS_PDF_LIB_PATH . '/config/lang/' . _LANGCODE . '.php';
+if( file_exists( $filename ) ) {
   include_once $filename;
 } else {
-  include_once ICMS_PDF_LIB_PATH.'/config/lang/en.php';
+  include_once ICMS_PDF_LIB_PATH . '/config/lang/en.php';
 }
 
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true);
@@ -99,7 +99,7 @@ $pdf -> setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
 $pdf -> setLanguageArray($l); //set language items
 
 // set font
-$pdf -> SetFont("dejavusans", "BI", 16);
+$pdf -> SetFont("dejavusans", "BI", 10);
 
 //initialize document
 $pdf -> AliasNbPages();
