e29e0287cf4acbe89cfc431b87df184d410d2fae
Ronald <pietjebell31@hotmail.com>
2008-08-21 19:58:52 +0000

    Moved div-tag from php-file to template
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk/modules/impression@4491 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- blocks/impression_spot.php
+++ blocks/impression_spot.php
@@ -29,16 +29,15 @@ function checkImpressionSpotgroups( $cid = 0, $permType = 'ImpressionCatPerm', $
 function b_impression_displayrssicons() {
     $mydirname = basename( dirname( dirname( __FILE__ ) ) );
     $modhandler = xoops_gethandler( 'module' );
-
-    $icons = '<div align="center" style="padding: 2px;">';
-
+	$icons='';
+	
     // Display rss icons if RSSFit module is installed
     // Plugin and sub-feed need to be activated!!
     $rss_mod = $modhandler -> getByDirName( 'rss' );
     if ( !$rss_mod ) {
       $rss_mod = false;
     } else {
-      $icons .= '<a href="'. ICMS_URL . '/modules/rss/rss.php?feed=' . $mydirname . '" alt="Get RSS news feed" target="_blank"><img src="'. ICMS_URL . '/modules/' . $mydirname . '/images/icon/rss.png" /></a>&nbsp;';
+      $icons = '<a href="'. ICMS_URL . '/modules/rss/rss.php?feed=' . $mydirname . '" alt="Get RSS news feed" target="_blank"><img src="'. ICMS_URL . '/modules/' . $mydirname . '/images/icon/rss.png" /></a>&nbsp;';
       // Google
       $icons .= '<a href="http://fusion.google.com/add?feedurl='. ICMS_URL . '/modules/rss/rss.php?feed=' . $mydirname . '"><img src="'. ICMS_URL . '/modules/' . $mydirname . '/images/rss_icons/google.png" alt="'._MB_IMPRESSION_ADDGOOGLE.'" title="'._MB_IMPRESSION_ADDGOOGLE.'" border="0"></a>&nbsp;';
       // Yahoo
@@ -49,7 +48,6 @@ function b_impression_displayrssicons() {
       $icons .= '<a href="http://feeds.my.aol.com/add.jsp?url='. ICMS_URL . '/modules/rss/rss.php?feed=' . $mydirname . '"><img src="'. ICMS_URL . '/modules/' . $mydirname . '/images/rss_icons/aol2.png" alt="'._MB_IMPRESSION_ADDAOL.'" title="'._MB_IMPRESSION_ADDAOL.'" border="0"></a>&nbsp;';
       // Windows Live
       $icons .= '<a href="http://www.live.com/?add='. ICMS_URL . '/modules/rss/rss.php?feed=' . $mydirname . '"><img style="width: 92px; height: 17px;" src="'. ICMS_URL . '/modules/' . $mydirname . '/images/rss_icons/windowslive.png" alt="'._MB_IMPRESSION_ADDMSLIVE.'" title="'._MB_IMPRESSION_ADDMSLIVE.'" border="0"></a>';
-      $icons .= '</div>';
     }
 
     return $icons;
@@ -57,21 +55,21 @@ function b_impression_displayrssicons() {
 
 function b_impression_spoticons($aid, $dirname) {
 
-        $iconadmin = '<a href="' . ICMS_URL . '/modules/' . $dirname . '/admin/index.php"><img src="' . ICMS_URL . '/modules/' . $dirname . '/images/icon/computer_small.png" alt="' . _MB_IMPRESSION_ADMINSECTION . '" title="' . _MB_IMPRESSION_ADMINSECTION . '" align="absmiddle"/></a>';
-        $iconadmin .= '&nbsp;<a href="' . ICMS_URL . '/modules/' . $dirname . '/admin/index.php?op=edit&amp;aid=' . $aid . '"><img src="' . ICMS_URL . '/modules/' . $dirname . '/images/icon/pageedit_small.png" alt="' . _MB_IMPRESSION_EDIT . '" title="' . _MB_IMPRESSION_EDIT . '" align="absmiddle"/></a>&nbsp;';
-        $iconadmin .= '<a href="' . ICMS_URL . '/modules/' . $dirname . '/admin/index.php?op=delete&amp;aid=' . $aid . '"><img src="' . ICMS_URL . '/modules/' . $dirname . '/images/icon/pagedelete_small.png" alt="' . _MB_IMPRESSION_DELETE . '" title="' . _MB_IMPRESSION_DELETE . '" align="absmiddle"/></a>&nbsp;';
+	$iconsadmin = '<a href="' . ICMS_URL . '/modules/' . $dirname . '/admin/index.php"><img src="' . ICMS_URL . '/modules/' . $dirname . '/images/icon/computer_small.png" alt="' . _MB_IMPRESSION_ADMINSECTION . '" title="' . _MB_IMPRESSION_ADMINSECTION . '" align="absmiddle"/></a>';
+	$iconsadmin .= '&nbsp;<a href="' . ICMS_URL . '/modules/' . $dirname . '/admin/index.php?op=edit&amp;aid=' . $aid . '"><img src="' . ICMS_URL . '/modules/' . $dirname . '/images/icon/pageedit_small.png" alt="' . _MB_IMPRESSION_EDIT . '" title="' . _MB_IMPRESSION_EDIT . '" align="absmiddle"/></a>&nbsp;';
+	$iconsadmin .= '<a href="' . ICMS_URL . '/modules/' . $dirname . '/admin/index.php?op=delete&amp;aid=' . $aid . '"><img src="' . ICMS_URL . '/modules/' . $dirname . '/images/icon/pagedelete_small.png" alt="' . _MB_IMPRESSION_DELETE . '" title="' . _MB_IMPRESSION_DELETE . '" align="absmiddle"/></a>&nbsp;';
 
-        return $iconadmin;
+	return $iconsadmin;
 }
 
-// Function: b_xoopstube_top_show
-// Input   : $options[0] = date for the most recent videos
-// 			   hits for the most popular videos
+// Function: b_impression_spot_show
+// Input   : $options[0] = 	date for the most recent videos
+// 			   			hits for the most popular videos
 //           $block['content'] = The optional above content
 //           $options[1] = How many videos are displayes
 //           $options[2] = Set date format
 //           $options[3] = Display RSS icons y/n
-// Output  : Returns the most recent or most popular videos
+// Output  : Returns the latest articles
 function b_impression_spot_show( $options ) {
 
     global $xoopsDB, $xoopsModuleConfig, $xoopsUser, $xoopsTpl;
@@ -84,38 +82,38 @@ function b_impression_spot_show( $options ) {
     $moderate = 0;
     $impressionmyts = &MyTextSanitizer :: getInstance();
 
-    $sql = $xoopsDB -> query( "SELECT aid, cid, title, submitter, published, status, hits, introtext FROM " . $xoopsDB -> prefix( 'impression_articles' ) . " WHERE published > 0 AND published <= " . time() . " AND status=0 ORDER BY published DESC", $options[1], 0 );
+    $sql = $xoopsDB -> query( "SELECT aid, cid, title, submitter, published, status, hits, introtext FROM " . $xoopsDB -> prefix( 'impression_articles' ) . " WHERE published > 0 AND published <= " . time() . " AND status=0 ORDER BY published DESC ", $options[1], 0 );
     while ( $myrow = $xoopsDB -> fetchArray( $sql ) ) {
         if ( false == checkImpressionSpotgroups( $myrow['cid'] ) || $myrow['cid'] == 0 ) {
             continue;
         }
 
         $articleload = array();
-        $title = $impressionmyts -> htmlSpecialChars( $impressionmyts -> stripSlashesGPC($myrow["title"]) );
+        $title = $impressionmyts -> htmlSpecialChars( $impressionmyts -> stripSlashesGPC( $myrow["title"] ) );
         $articleload['isadmin'] = ( ( is_object( $xoopsUser ) && !empty( $xoopsUser ) ) && $xoopsUser -> isAdmin( $impressionModule -> mid() ) ) ? true : false;
         $articleload['adminarticle'] = '';
         if ( $articleload['isadmin'] == true && $moderate == 0 ) {
-        $articleload['adminarticle'] = '<a href="' . ICMS_URL . '/modules/' . $impressionModule -> getVar( 'dirname' ) . '/admin/index.php"><img src="' . ICMS_URL . '/modules/' . $impressionModule -> getVar( 'dirname' ) . '/images/icon/computer.png" alt="' . _MB_IMPRESSION_ADMINSECTION . '" title="' . _MB_IMPRESSION_ADMINSECTION . '" align="absmiddle"/></a>&nbsp;';
-        $articleload['adminarticle'] .= '<a href="' . ICMS_URL . '/modules/' . $impressionModule -> getVar( 'dirname' ) . '/admin/index.php?op=edit&amp;aid=' . $myrow['aid'] . '"><img src="' . ICMS_URL . '/modules/' . $impressionModule -> getVar( 'dirname' ) . '/images/icon/page_edit.png" alt="' . _MB_IMPRESSION_EDIT . '" title="' . _MB_IMPRESSION_EDIT . '" align="absmiddle"/></a>&nbsp;';
-        $articleload['adminarticle'] .= '<a href="' . ICMS_URL . '/modules/' . $impressionModule -> getVar( 'dirname' ) . '/admin/index.php?op=delete&amp;aid=' . $myrow['aid'] . '"><img src="' . ICMS_URL . '/modules/' . $impressionModule -> getVar( 'dirname' ) . '/images/icon/page_delete.png" alt="' . _MB_IMPRESSION_DELETE . '" title="' . _MB_IMPRESSION_DELETE . '" align="absmiddle"/></a>';
+			$articleload['adminarticle'] = '<a href="' . ICMS_URL . '/modules/' . $impressionModule -> getVar( 'dirname' ) . '/admin/index.php"><img src="' . ICMS_URL . '/modules/' . $mydirname . '/images/icon/computer.png" alt="' . _MB_IMPRESSION_ADMINSECTION . '" title="' . _MB_IMPRESSION_ADMINSECTION . '" align="absmiddle"/></a>&nbsp;';
+			$articleload['adminarticle'] .= '<a href="' . ICMS_URL . '/modules/' . $mydirname . '/admin/index.php?op=edit&amp;aid=' . $myrow['aid'] . '"><img src="' . ICMS_URL . '/modules/' . $mydirname . '/images/icon/page_edit.png" alt="' . _MB_IMPRESSION_EDIT . '" title="' . _MB_IMPRESSION_EDIT . '" align="absmiddle"/></a>&nbsp;';
+			$articleload['adminarticle'] .= '<a href="' . ICMS_URL . '/modules/' . $mydirname . '/admin/index.php?op=delete&amp;aid=' . $myrow['aid'] . '"><img src="' . ICMS_URL . '/modules/' . $mydirname . '/images/icon/page_delete.png" alt="' . _MB_IMPRESSION_DELETE . '" title="' . _MB_IMPRESSION_DELETE . '" align="absmiddle"/></a>';
 
         } else {
-        $articleload['adminarticle'] = '[ <a href="' . ICMS_URL . '/modules/' . $impressionModule -> getVar( 'dirname' ) . '/submit.php?op=edit&amp;aid=' . $myrow['aid'] . '&approve=1">' . _MB_IMPRESSION_APPROVE . '</a> | ';
-        $articleload['adminarticle'] .= '<a href="' . ICMS_URL . '/modules/' . $impressionModule -> getVar( 'dirname' ) . '/submit.php?op=delete&amp;aid=' . $myrow['aid'] . '">' . _MB_IMPRESSION_DELETE . '</a> ]';
+			$articleload['adminarticle'] = '[ <a href="' . ICMS_URL . '/modules/' . $mydirname . '/submit.php?op=edit&amp;aid=' . $myrow['aid'] . '&approve=1">' . _MB_IMPRESSION_APPROVE . '</a> | ';
+			$articleload['adminarticle'] .= '<a href="' . ICMS_URL . '/modules/' . $mydirname . '/submit.php?op=delete&amp;aid=' . $myrow['aid'] . '">' . _MB_IMPRESSION_DELETE . '</a> ]';
         }
 
         $articleload['id'] = intval($myrow['aid']);
         $articleload['cid'] = intval($myrow['cid']);
         $articleload['title'] = $title;
-        $articleload['title'] = '<a href="' . ICMS_URL . '/modules/' . $impressionModule -> getVar( 'dirname' ) . '/singlearticle.php?cid=' . intval($myrow['cid']) . '&amp;aid=' . intval($myrow['aid']) . '">' . $title . ' </a>';
+        $articleload['title'] = '<a href="' . ICMS_URL . '/modules/' . $mydirname . '/singlearticle.php?cid=' . intval($myrow['cid']) . '&amp;aid=' . intval($myrow['aid']) . '">' . $title . ' </a>';
         $articleload['date'] = formatTimestamp( $myrow['published'], $options[2] );
         $articleload['hits'] = sprintf( _MB_IMPRESSION_ARTICLEHITS, intval( $myrow['hits'] ) );
         $articleload['submitter'] = xoops_getLinkedUnameFromId($myrow['submitter']);
         $articleload['introtext'] = $impressionmyts -> displayTarea( $myrow['introtext'], 1, 1, 1, 1, 1 );
-        $articleload['readmore'] = '<a href="' . ICMS_URL . '/modules/' . $impressionModule -> getVar( 'dirname' ) . '/singlearticle.php?cid=' . intval($myrow['cid']) . '&amp;aid=' . intval($myrow['aid']) . '">' . _MB_IMPRESSION_READMORE . '</a>';
+        $articleload['readmore'] = '<a href="' . ICMS_URL . '/modules/' . $mydirname . '/singlearticle.php?cid=' . intval($myrow['cid']) . '&amp;aid=' . intval($myrow['aid']) . '">' . _MB_IMPRESSION_READMORE . '</a>';
         $articleload['rssicons'] = $options[3];
         $articleload['showrss'] = b_impression_displayrssicons();
-        $xoopsTpl -> assign( 'dirname', $impressionModule -> getVar( 'dirname' ) );
+        $xoopsTpl -> assign( 'dirname', $mydirname );
         $block['article'][] = $articleload;
     }
     unset( $_block_check_array );
@@ -123,7 +121,7 @@ function b_impression_spot_show( $options ) {
     return $block;
 } 
 
-// b_xoopstube_top_edit()
+// b_impresson_top_edit()
 // @param $options
 // @return
 function b_impression_spot_edit( $options ) {
