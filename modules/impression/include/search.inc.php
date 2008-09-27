d43db42d94e3478e30eb316400f9ba442385bd1d
Ronald <pietjebell31@hotmail.com>
2008-09-27 23:01:38 +0000

    Replaced 'ORDER BY date DESC' with 'ORDER BY published DESC'
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk@5193 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/impression/include/search.inc.php
+++ modules/impression/include/search.inc.php
@@ -44,14 +44,18 @@ function impression_search( $queryarray, $andor, $limit, $offset, $userid ) {
     // because count() returns 1 even if a supplied variable
     // is not an array, we must check if $querryarray is really an array
     if ( is_array( $queryarray ) && $count = count( $queryarray ) ) {
-        $sql .= " AND ((title LIKE LOWER('%$queryarray[0]%') OR LOWER(introtext) LIKE LOWER('%$queryarray[0]%') OR LOWER(description) LIKE LOWER('%$queryarray[0]%'))";
+        $sql .= " AND ((title LIKE LOWER('%$queryarray[0]%') OR LOWER(introtext) 
+							  LIKE LOWER('%$queryarray[0]%') OR LOWER(description) 
+							  LIKE LOWER('%$queryarray[0]%'))";
         for( $i = 1;$i < $count;$i++ ) {
             $sql .= " $andor ";
-            $sql .= "(title LIKE LOWER('%$queryarray[$i]%') OR LOWER(introtext) LIKE LOWER('%$queryarray[$i]%') OR LOWER(description) LIKE LOWER('%$queryarray[$i]%'))";
+            $sql .= "(title LIKE LOWER('%$queryarray[$i]%') OR LOWER(introtext) 
+							LIKE LOWER('%$queryarray[$i]%') OR LOWER(description) 
+							LIKE LOWER('%$queryarray[$i]%'))";
         } 
         $sql .= ") ";
     } 
-    $sql .= "ORDER BY date DESC";
+    $sql .= "ORDER BY published DESC";
     $result = $xoopsDB -> query( $sql, $limit, $offset );
     $ret = array();
     $i = 0;
