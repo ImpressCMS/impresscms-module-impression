6f12182e60f6bcf19855f1704506680300140a7a
Ronald <pietjebell31@hotmail.com>
2008-10-27 12:57:31 +0000

    Revamped administration menu. Added unofficial translations of GNU GPL License. Updated About page.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk/modules/impression@6053 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- admin/indexpage.php
+++ admin/indexpage.php
@@ -31,7 +31,7 @@ switch ( strtolower( $op ) ) {
             XoopsErrorHandler_HandleError( E_USER_WARNING, $sql, __FILE__, __LINE__ );
             return false;
         } 
-        redirect_header( "index.php", 1, _AM_IMPRESSION_IPAGE_UPDATED );
+        redirect_header( 'index.php', 1, _AM_IMPRESSION_IPAGE_UPDATED );
         break;
 
     default:
@@ -43,7 +43,7 @@ switch ( strtolower( $op ) ) {
         list( $indeximage, $indexheading, $indexheader, $indexfooter, $indexheaderalign, $indexfooteralign, $lastarticlesyn, $lastarticlestotal ) = $xoopsDB -> fetchrow( $result );
 
         xoops_cp_header();
-        impression_adminmenu( "<h4>" . _AM_IMPRESSION_INDEXPAGE . "</h4>" );
+        impression_adminmenu( 2, _AM_IMPRESSION_INDEXPAGE );
 
         echo "   <div style='padding:5px; background-color: #EEEEEE; border: 1px solid #D9D9D9;'>
 		 <span style='font-weight: bold; color: #0A3760;'>" . _AM_IMPRESSION_IPAGE_INFORMATION . "</span>\n
--- admin/menu.php
+++ admin/menu.php
@@ -4,13 +4,45 @@
  * Module: Impression
  */
 
+$impressiondir = basename( dirname( dirname( __FILE__ ) ) );
+
+include_once ICMS_ROOT_PATH . '/modules/' . $impressiondir . '/include/functions.php';
+
 $adminmenu[1]['title'] = _MI_IMPRESSION_BINDEX;
 $adminmenu[1]['link']="admin/index.php";
-//$adminmenu[2]['title'] = _MI_IMPRESSION_INDEXPAGE;
-//$adminmenu[2]['link']="admin/indexpage.php";
+$adminmenu[2]['title'] = _MI_IMPRESSION_INDEXPAGE;
+$adminmenu[2]['link']="admin/indexpage.php";
 $adminmenu[3]['title'] = _MI_IMPRESSION_MCATEGORY;
 $adminmenu[3]['link']="admin/category.php";
 $adminmenu[4]['title'] = _MI_IMPRESSION_MARTICLES;
 $adminmenu[4]['link']="admin/index.php?op=edit";
 
+global $xoopsModule, $xoopsConfig;
+
+if ( isset( $xoopsModule ) ) {
+
+	if ( file_exists( ICMS_ROOT_PATH . '/modules/' . $impressiondir . '/language/' . $xoopsConfig['language'] . '/admin.php' ) ) {
+		include_once ICMS_ROOT_PATH . '/modules/' . $impressiondir . '/language/' . $xoopsConfig['language'] . '/admin.php';
+	} else {
+		include_once ICMS_ROOT_PATH . '/modules/' . $impressiondir . '/language/english/admin.php';
+	}
+
+	$i = -1;
+
+	$i++;
+	$headermenu[$i]['title'] = _PREFERENCES;
+	$headermenu[$i]['link']  = '../../system/admin.php?fct=preferences&amp;op=showmod&amp;mod=' . $xoopsModule -> getVar( 'mid' );
+
+	$i++;
+	$headermenu[$i]['title'] = _AM_IMPRESSION_GOMODULE;
+	$headermenu[$i]['link']  = ICMS_URL . '/modules/' . $impressiondir;
+
+	$i++;
+	$headermenu[$i]['title'] = _AM_IMPRESSION_BUPDATE;
+	$headermenu[$i]['link']  = ICMS_URL . '/modules/system/admin.php?fct=modulesadmin&op=update&module=' . $impressiondir;
+
+	$i++;
+	$headermenu[$i]['title'] = _AM_IMPRESSION_ABOUT;
+	$headermenu[$i]['link']  = ICMS_URL . '/modules/' . $impressiondir . '/admin/about.php';
+}
 ?>
