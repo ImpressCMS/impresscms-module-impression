03a58b006defab9bec5533810c5fef9fea7b299a
GibaPhp <gibaphp.xoopstotal@gmail.com>
2008-10-28 20:06:24 +0000

    portuguesebr - ImPression - All Revision and translation - Step 9
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk/modules/impression@6116 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- language/portuguesebr/blocks.php
+++ language/portuguesebr/blocks.php
@@ -9,27 +9,26 @@
  */
  
 // Blocks
-// Blocks
-define( '_MB_IMPRESSION_DISP', '<b>Display: </b>' );
-define( '_MB_IMPRESSION_FILES', 'Articles' );
-define( '_MB_IMPRESSION_CHARS', '<b>Length of the title: </b>' );
-define( '_MB_IMPRESSION_LENGTH', ' characters' );
-define( '_MB_IMPRESSION_EDIT', 'Edit article' );
-define( '_MB_IMPRESSION_DELETE', 'Delete article' );
-define( '_MB_IMPRESSION_APPROVE', 'Approve' );
-define( '_MB_IMPRESSION_CATEGORY', 'Category' );
+define( '_MB_IMPRESSION_DISP', '<b>Exibir: </b>' );
+define( '_MB_IMPRESSION_FILES', 'Artigos' );
+define( '_MB_IMPRESSION_CHARS', '<b>Comprimento do título: </b>' );
+define( '_MB_IMPRESSION_LENGTH', ' Caracteres' );
+define( '_MB_IMPRESSION_EDIT', 'Editar artigo' );
+define( '_MB_IMPRESSION_DELETE', 'Apagar artigo' );
+define( '_MB_IMPRESSION_APPROVE', 'Aprovar' );
+define( '_MB_IMPRESSION_CATEGORY', 'Categoria' );
 define( '_MB_IMPRESSION_ARTICLE', 'Título do Artigo' );
-define( '_MB_IMPRESSION_DATE', 'Date' );
-define( '_MB_IMPRESSION_HEADLINES', 'headlines' );
+define( '_MB_IMPRESSION_DATE', 'Data' );
+define( '_MB_IMPRESSION_HEADLINES', 'manchetes' );
 define( '_MB_IMPRESSION_ADMINSECTION', 'Setor Administrativo' );
 define( '_MB_IMPRESSION_DATEFORMAT', '<b>Formato de Data: </b>' );
-define( '_MB_IMPRESSION_DATEFORMATMANUAL', 'See <a href="http://jp.php.net/manual/en/function.date.php" target="_blank">PHP manual</a>' );
-define( '_MB_IMPRESSION_SELECTFEED', '<b>Select RSS feed: </b>' );
-define( '_MB_IMPRESSION_SELECTFEED_DSC', '<small><br /><br /> - Select ICMS to use ImpressCMS RSS feed.<br /> - Select RSSfit to use Brandycoke\'s RSSfit module (subfeed needs to be activated).</small>'  );
-define( '_MB_IMPRESSION_READMORE', 'Read more' );
-define( "_MB_IMPRESSION_PUBLISHEDON", "Published on " );
+define( '_MB_IMPRESSION_DATEFORMATMANUAL', 'Veja também o <a href="http://pt.php.net/manual/pt_BR/function.date.php" target="_blank">Manual do PHP</a>' );
+define( '_MB_IMPRESSION_SELECTFEED', '<b>Selecione o RSS feed: </b>' );
+define( '_MB_IMPRESSION_SELECTFEED_DSC', '<small><br /><br /> - Selecione o ICMS para uso no ImpressCMS RSS.<br /> - Escolha RSSfit para usar o módulo Brandycoke RSSfit (subfeed precisa ser ativado).</small>'  );
+define( '_MB_IMPRESSION_READMORE', 'Leia Mais' );
+define( "_MB_IMPRESSION_PUBLISHEDON", "Publicado em " );
 define( "_MB_IMPRESSION_BY", " por " );
-define( "_MB_IMPRESSION_ARTICLEHITS", "(%s reads)" );
+define( "_MB_IMPRESSION_ARTICLEHITS", "(%s leituras)" );
 define( '_MB_IMPRESSION_FEED', 'RSS feed' );
 define( '_MB_IMPRESSION_ICMSFEED', 'ICMS' );
 define( '_MB_IMPRESSION_RSSFITFEED', 'RSSfit' );
