59501d116a8391c3a77c6a583f985e3dbd0f46f6
Ronald <pietjebell31@hotmail.com>
2008-10-29 21:33:51 +0000

    Fixed block settings
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk/modules/impression@6148 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- include/onupdate.inc.php
+++ include/onupdate.inc.php
@@ -1,23 +1,19 @@
 <?php
 /**
  * $Id: onupdate.inc.php
- * Module: Impression
+ * Module: imGlossary
  */
 
-if( ! defined( 'ICMS_ROOT_PATH' ) ) exit ;
+if ( ! defined( 'ICMS_ROOT_PATH' ) ) exit;
 
 // referer check
 $ref = xoops_getenv('HTTP_REFERER');
-if( $ref == '' || strpos( $ref , ICMS_URL.'/modules/system/admin.php' ) === 0 ) {
+if ( $ref == '' || strpos( $ref, ICMS_URL . '/modules/system/admin.php' ) === 0 ) {
 	/* module specific part */
-
-
 
 	/* General part */
 
 	// Keep the values of block's options when module is updated (by nobunobu)
-	include dirname( __FILE__ ) . "/updateblock.inc.php" ;
-
+	include dirname( __FILE__ ) . "/updateblock.inc.php";
 }
-
 ?>
