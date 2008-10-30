1e47ce3edde1935ec5417930a4ea1d0b7160175e
Ronald <pietjebell31@hotmail.com>
2008-10-30 00:11:47 +0000

    Removed function impression_substr
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk/modules/impression@6151 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- include/functions.php
+++ include/functions.php
@@ -767,64 +767,6 @@ function impression_updateCounter($aid) {
          $result = $xoopsDB -> queryF( $sql );
 }
 
-function impression_substr($str, $start, $length, $trimmarker = '...') {
-	$config_handler =& xoops_gethandler('config');
-	$im_multilanguageConfig =& $config_handler->getConfigsByCat(IM_CONF_MULILANGUAGE);
-    
-	if ($im_multilanguageConfig['ml_enable']) {
-		$tags = explode(',',$im_multilanguageConfig['ml_tags']);
-		$strs = array();
-		$hasML = false;
-		foreach ($tags as $tag){
-			if (preg_match("/\[".$tag."](.*)\[\/".$tag."\]/sU",$str,$matches)) {
-				if (count($matches) > 0){
-					$hasML = true;
-					$strs[] = $matches[1];
-				}
-			}
-		}
-	} else {
-		$hasML = false;
-	}
-	
-	if (!$hasML) {
-        $strs = array($str);
-	}
-	
-	for ($i = 0; $i <= count($strs)-1; $i++) {
-		if ( !XOOPS_USE_MULTIBYTES ) {
-			$strs[$i] = ( strlen($strs[$i]) - $start <= $length ) ? substr( $strs[$i], $start, $length ) : substr( $strs[$i], $start, $length - strlen($trimmarker) ) . $trimmarker;
-		}
-
-		if (function_exists('mb_internal_encoding') && @mb_internal_encoding(_CHARSET)) {
-			$str2 = mb_strcut( $strs[$i] , $start , $length - strlen( $trimmarker ) );
-			$strs[$i] = $str2 . ( mb_strlen($strs[$i])!=mb_strlen($str2) ? $trimmarker : '' );
-		}
-
-		// phppp patch
-		$DEP_CHAR=127;
-		$pos_st=0;
-		$action = false;
-		for ( $pos_i = 0; $pos_i < strlen($strs[$i]); $pos_i++ ) {
-			if ( ord( substr( $strs[$i], $pos_i, 1) ) > 127 ) {
-				$pos_i++;
-			}
-			if ($pos_i<=$start) {
-				$pos_st=$pos_i;
-			}
-			if ($pos_i>=$pos_st+$length) {
-				$action = true;
-				break;
-			}
-		}
-		$strs[$i] = ($action) ? substr( $strs[$i], $pos_st, $pos_i - $pos_st - strlen($trimmarker) ) . $trimmarker : $strs[$i];
-
-		$strs[$i] = ($hasML)?'['.$tags[$i].']'.$strs[$i].'[/'.$tags[$i].']':$strs[$i];
-	}
-	$str = implode('',$strs);
-
-	return $str;
-}
 // Check if imGlossary module is installed
 function impression_imglossary_module_included() {
   global $xoopsModuleConfig;
