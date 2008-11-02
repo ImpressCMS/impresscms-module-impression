30115c7ca765e84e10753754bd7dd31164ce302f
xoops-magazine <xoops-magazine@tld.com>
2008-11-02 17:40:11 +0000

    German translation, thanks to @Prickel!
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk/modules/impression@6331 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/german/mail_template/video_approve_notify.tpl
@@ -0,0 +1,21 @@
+Hallo {X_UNAME},
+
+Das eingereichte Video "{LINK_NAME}" auf {X_SITENAME} wurde freigegeben.
+
+Sie können das Video hier anschauen:
+{LINK_URL}
+
+-----------
+
+Sie erhalten diese Nachricht, weil Sie ausgewählt hatten, benachrichtigt zu werden, wenn dieses Video freigeben wurde.
+
+Wenn es sich um eine irrtümliche Nachrticht handelt, oder Sie künftig keine weiteren Nachrichten erhalten möchten, ändern Sie bitte Ihr Abo durch Besuch des nachfolgenden Link:
+{X_UNSUBSCRIBE_URL}
+
+Bitte antworten Sie nicht auf diese Nachricht.
+
+-----------
+
+{X_SITENAME} ({X_SITEURL}) 
+webmaster
+{X_ADMINMAIL}
---
+++ language/german/main.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * $Id: main.php
+ * Module: Impression
+ * Language: German
+ * Format: UTF-8
+ * Author: Author: Prickel + sato-san
+ */
+
+define( "_MD_IMPRESSION_NOARTICLE", "Dieser Beitrag existiert nicht!" );
+define( "_MD_IMPRESSION_SUBCATLISTING", "Kategorienauflistung" );
+define( "_MD_IMPRESSION_ISADMINNOTICE", "Webmaster: es besteht ein Problem mit diesem Bild." );
+define( "_MD_IMPRESSION_THANKSFORINFO", "Vielen Dank für Ihre Erstellung." );
+define( "_MD_IMPRESSION_ISAPPROVED", "Vielen Dank für Ihre Erstellung.<br />Ihre Anfrage wurde geprüft und wird in unserer Auflistung hinzugefügt." );
+define( "_MD_IMPRESSION_THANKSFORHELP", "Vielen Dank für Ihre Hilfe, dieses Verzeichnis zu vervollständigen." );
+define( "_MD_IMPRESSION_FORSECURITY", "aus Sicherheitsgründen wird Ihr Name und Ihre IP-Adresse temporär aufgezeichnet." );
+define( "_MD_IMPRESSION_DESCRIPTION", "Beschreibung" );
+define( "_MD_IMPRESSION_SUBMITCATHEAD", "Eingabeformular für Beiträge" );
+define( "_MD_IMPRESSION_MAIN", "Home" );
+define( "_MD_IMPRESSION_POPULAR", "populär" );
+define( "_MD_IMPRESSION_NEWTHISWEEK", "neu diese Woche" );
+define( "_MD_IMPRESSION_UPTHISWEEK", "aktualisiert diese Woche" );
+define( "_MD_IMPRESSION_POPULARITYLTOM", "gelesen (wenig nach viel gelesene)" );
+define( "_MD_IMPRESSION_POPULARITYMTOL", "gelesen (viel nach wenig gelesene)" );
+define( "_MD_IMPRESSION_TITLEATOZ", "Titel (A bis Z)" );
+define( "_MD_IMPRESSION_TITLEZTOA", "Titel (Z bis A)" );
+define( "_MD_IMPRESSION_DATEOLD", "Datum (alte Artikel zuerst)" );
+define( "_MD_IMPRESSION_DATENEW", "Datum (neue Artikel zuerst)" );
+define( "_MD_IMPRESSION_INTROTEXTC", "Einführungstext: " );
+define( "_MD_IMPRESSION_INTROTEXTC_DSC", "<br />der hier eingegebene Text dient als 1. Teil des Beitrags und wird im Spotlightblock angezeigt" );
+define( "_MD_IMPRESSION_DESCRIPTIONC", "Hauptbeitrag: " );
+define( "_MD_IMPRESSION_DESCRIPTIONC_DSC", "<br />der hier eingegebene Text ist gemeinsam mit dem Einführungstext der komplette Beitrag" );
+define( "_MD_IMPRESSION_CATEGORYC", "Kategorie: " );
+define( "_MD_IMPRESSION_VERSION", "Version" );
+define( "_MD_IMPRESSION_SUBMITDATE", "veröffentlicht" );
+define( "_MD_IMPRESSION_ARTICLEHITS", "(%s gelesen)" );
+define( "_MD_IMPRESSION_PUBLISHERC", "Veröffentlicher: " );
+define( "_MD_IMPRESSION_MODIFY", "ändern" );
+define( "_MD_IMPRESSION_SUBMITBROKEN", "einreichen" );
+define( "_MD_IMPRESSION_TELLAFRIEND", "empfehlen" );
+define( "_MD_IMPRESSION_EDIT", "bearbeiten" );
+define( "_MD_IMPRESSION_THEREARE", "es sind <b>%s</b> <i>Kategorien</i> und <b>%s</b> <i>Beitrag/Beiträge</i> aufgelistet" );
+define( "_MD_IMPRESSION_THEREIS", "es werden <b>%s</b> <i>Kategorie</i> und <b>%s</b> <i>Beitrag/Beiträge</i> aufgelistet" );
+define( "_MD_IMPRESSION_LATESTLIST", "letzte Beiträge" );
+define( "_MD_IMPRESSION_FILETITLE", "Beiträgetitel: " );
+define( "_MD_IMPRESSION_ARTICLE_PUBLISHER", "Name des Beitragsveröffentlichers:" );
+define( "_MD_IMPRESSION_HOMEPAGEC", "Homepage: " );
+define( "_MD_IMPRESSION_NOTSPECIFIED", "nicht spezifiziert" );
+define( "_MD_IMPRESSION_PUBLISHER", "Veröffentlicher" );
+define( "_MD_IMPRESSION_UPDATEDON", "aktualisiert am" );
+define( "_MD_IMPRESSION_PRICEFREE", "frei" );
+define( "_MD_IMPRESSION_READMORE", "weiterlesen" );
+define( "_MD_IMPRESSION_OPTIONS", 'Optionen: ' );
+define( "_MD_IMPRESSION_NOTIFYAPPROVE", 'Informieren Sie mich, wenn dieser Beitrag freigegeben wurde' );
+define( "_MD_IMPRESSION_INTFILEFOUND", "Hier ist ein interessanter Beitrag zum Anschauen %s" ); // %s is your site name
+define( "_MD_IMPRESSION_CATEGORY", "Kategorie" );
+define( "_MD_IMPRESSION_HITS", "Ansichten" );
+define( "_MD_IMPRESSION_SORTBY", "Sortiert nach:" );
+define( "_MD_IMPRESSION_TITLE", "Titel" );
+define( "_MD_IMPRESSION_DATE", "Datum" );
+define( "_MD_IMPRESSION_POPULARITY", "gelesen" );
+define( "_MD_IMPRESSION_CURSORTBY", "Beiträge derzeit sortiert nach: %s" );
+define( "_MD_IMPRESSION_CANCEL", "abbrechen" );
+define( "_MD_IMPRESSION_MUSTREGFIRST", "Entschuldigung, Sie haben keine ausreichenden Rechte, um diese Aktion auszuführen.<br />Bitte registrieren oder zuerst anmelden!" );
+define( "_MD_IMPRESSION_SUBMITARTICLE", "eingereichter Beitrag" );
+define( "_MD_IMPRESSION_SUB_SNEWMNAMEDESC", "<ul><li>alle neuen Beiträge sind Gegenstand einer Überprüfung. Es kann bis zu 24 Stunden dauern, bevor sie in unserer Auflistung erscheinen.</li><li>Wir beschränken die Rechte, um jeden eingereichten Beitrag ggf. ablehnen oder Inhalt ändern zu können, damit diese nicht ungeprüft eingestellt werden.</li></ul>" );
+define( "_MD_IMPRESSION_MAINLISTING", "Hauptkategorien Auflistungen" );
+define( "_MD_IMPRESSION_LASTWEEK", "Letzte Woche" );
+define( "_MD_IMPRESSION_LAST30DAYS", "Die letzten 30 Tage" );
+define( "_MD_IMPRESSION_1WEEK", "1 Woche" );
+define( "_MD_IMPRESSION_2WEEKS", "2 Wochen" );
+define( "_MD_IMPRESSION_30DAYS", "30 Tage" );
+define( "_MD_IMPRESSION_SHOW", "zeige" );
+define( "_MD_IMPRESSION_DAYS", "Tage" );
+define( "_MD_IMPRESSION_NEWlinks", "Neue Beiträge" );
+define( "_MD_IMPRESSION_TOTALNEWARTICLES", "alle neuen Beiträge" );
+define( "_MD_IMPRESSION_DTOTALFORLAST", "alle neuen Beiträge seit dem letzten mal" );
+define( "_MD_IMPRESSION_AGREE", "ich stimme zu" );
+define( "_MD_IMPRESSION_DOYOUAGREE", "stimmen Sie den oben genannten Bedingungen zu?" );
+define( "_MD_IMPRESSION_DISCLAIMERAGREEMENT", "Haftungsschluss" );
+define( "_MD_IMPRESSION_RESOURCEID", "Resourcen-ID#: " );
+define( "_MD_IMPRESSION_REPORTER", "Original Berichterstatter: " );
+define( "_MD_IMPRESSION_DATEREPORTED", "gemeldetes Datum: " );
+define( "_MD_IMPRESSION_BROWSETOTOPIC", "<b>zeige Beiträge in alphabetischer Reihenfolge</b>" );
+
+define( "_MD_IMPRESSION_DELETE", "löschen" );
+define( "_MD_IMPRESSION_DISPLAYING", "angezeigt durch: " );
+define( "_MD_IMPRESSION_LEGENDTEXTNEW", "heute neu" );
+define( "_MD_IMPRESSION_LEGENDTEXTNEWTHREE", "neu in den letzten 3 Tagen" );
+define( "_MD_IMPRESSION_LEGENDTEXTTHISWEEK", "neu diese Woche" );
+define( "_MD_IMPRESSION_LEGENDTEXTNEWLAST", "über 1 Woche" );
+define( "_MD_IMPRESSION_THISFILEDOESNOTEXIST", "Warnung: dieser Beitrag existiert nicht!" );
+define( "_MD_IMPRESSION_BROKENREPORTED", "defekter Beitrag gemeldet" );
+
+define( "_MD_IMPRESSION_REV_SNEWMNAMEDESC", " 
+bitte füllen Sie das untere Formular komplett aus, und wir werden Ihre Einreichung so schnell wie möglich hinzufügen.<br /><br />
+vielen Dank dafür, das Sie sich Zeit genommen haben, Ihre Meinung einzureichen. Wir möchten unseren Benutzern die Möglichkeit geben, uns zu helfen, die Qualität unserer Software zu verbessern.<br /><br />Alle Artikel müssen von einem unserer Webmastern überprüft werden, bevor sie es auf die Webseite setzen. 
+" );
+define( "_MD_IMPRESSION_ISNOTAPPROVED", "Ihre Einreichung muß zuerst durch einen Moderator überprüft werden." );
+define( "_MD_IMPRESSION_HOMEPAGETITLEC", "Homepagetitel: " );
+define( "_MD_IMPRESSION_SCREENSHOTCLICK", "Vollbild anzeigen" );
+define( "_MD_IMPRESSION_OTHERBYUID", "andere Beiträge von: " );
+define( "_MD_IMPRESSION_NOOTHERBYUID", "keine anderen Beiträge von: " );
+define( "_MD_IMPRESSION_LINKTIMES", "Beitragszeiten: " );
+define( "_MD_IMPRESSION_MAINTOTAL", "Alle Beiträge: " );
+define( "_MD_IMPRESSION_ARTICLENOW", "Zeige Beiträge" );
+define( "_MD_IMPRESSION_PAGES", "<b>Seiten</b>" );
+define( "_MD_IMPRESSION_SUBMITTER", "Einreichender" );
+define( "_MD_IMPRESSION_ERROR", "Fehler bei der Aktualisierung der Datenbank: die Information wurde nicht gespeichert" );
+define( "_MD_IMPRESSION_COPYRIGHT", "Copyright" );
+
+// added frankblack
+define( "_MD_IMPRESSION_NOTALLOWESTOSUBMIT", "Sie sind nicht berechtigt, Beiträge einzureichen" );
+define( "_MD_IMPRESSION_INFONOSAVEDB", "Information nicht in der Datenbank gespeichert: <br /><br />" );
+
+define( "_MD_IMPRESSION_NEWLAST", "neu eingereicht vorletzte Woche" );
+define( "_MD_IMPRESSION_NEWTHIS", "neu eingereicht diese Woche" );
+define( "_MD_IMPRESSION_THREE", "neu eingereicht in den letzten 3 Tagen" );
+define( "_MD_IMPRESSION_TODAY", "heute neu eingereicht" );
+define( "_MD_IMPRESSION_NO_FILES", "derzeit keine Beiträge" );
+
+define( "_MD_IMPRESSION_NOPERMISSIONTOPOST", "Sie haben keine Berechtigung, um Beiträge in dieser Kategorie zu erstellen." );
+define( "_MD_IMPRESSION_RESOURCES", "Resourcen" );
+
+define( "_MD_IMPRESSION_PUBLISHDATE", "Veröffentlicht" );
+define( "_MD_IMPRESSION_APPROVE", "überprüfen" );
+define( "_MD_IMPRESSION_MODERATOR_OPTIONS", "Moderatorenoptionen" );
+
+// added by McDonald
+define( "_MD_IMPRESSION_KEYWORDS", "Stichwörter:" );
+define( "_MD_IMPRESSION_KEYWORDS_NOTE", "Stichwörter sollten durch ein Komma getrennt werden<br />(<i>Stichwort1, Stichwort2, Stichwort3, etc.</i>)" );
+define( "_MD_IMPRESSION_NOARTICLELOAD", "Vielen Dank für Ihren Beitrag!" );
+define( "_MD_IMPRESSION_LINKID", "Artikel-ID" );
+define( "_MD_IMPRESSION_ADDTO", "hinzufügen zu: " );
+define( "_MD_IMPRESSION_NEWARTICLES", "letzte Beiträge" );
+define( "_MD_IMPRESSION_TAKINGUBACK", "bringt Sie zurück." );
+define( "_MD_IMPRESSION_PRINT", "drucke Beitrag" );
+define( "_MD_IMPRESSION_PDF", "PDF" );
+define( "_MD_IMPRESSION_PUBLISHEDON", "veröffentlicht am " );
+define( "_MD_IMPRESSION_BY", " durch " );
+define( "_MD_IMPRESSION_NOITEMSELECTED", "Sie haben keinen gültigen Beitrag ausgewählt!");
+define( "_MD_IMPRESSION_ARTICLESCAT", " Beitrag" );
+define( "_MD_IMPRESSION_ADMINSECTION", "administrativer Bereich" );
+define( "_MD_IMPRESSION_ARTICLE", "Beitragstitel" );
+define( "_MD_IMPRESSION_WHO_WHEN", "Veröffentlicht durch %s am %s" );
+define( "_MD_IMPRESSION_RANK", "Rangliste" );
+//define( "_MD_IMPRESSION_EDIT", "Edit article" );
+//define( "_MD_IMPRESSION_DELETE", "Delete article" );
+define( '_MD_IMPRESSION_FEED', 'RSS feed' );
+?>
