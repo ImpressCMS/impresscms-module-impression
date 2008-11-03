2154e427b6f4175846d15354db6357c9d716905a
Ronald <pietjebell31@hotmail.com>
2008-11-03 12:25:25 +0000

    Updated tag plugin
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk/modules/impression@6354 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- include/plugin.tag.php
+++ include/plugin.tag.php
@@ -10,9 +10,7 @@
  * @package		module::tag
  */
 
-if (!defined("XOOPS_ROOT_PATH")) {
-	die("ICMS root path not defined");
-}
+if ( !defined( 'ICMSS_ROOT_PATH' ) ) { die( 'ICMS root path not defined' ); }
 
 /**
  * Get item fields:
@@ -32,11 +30,11 @@ if (!defined("XOOPS_ROOT_PATH")) {
 
 
 
-function impression_tag_iteminfo(&$items) {
+function impression_tag_iteminfo( &$items ) {
   
     $mydirname = basename( dirname( dirname( __FILE__ ) ) );
 
-    if (empty($items) || !is_array($items)) {
+    if ( empty( $items ) || !is_array( $items ) ) {
         return false; 
     } 
 
@@ -45,25 +43,25 @@ function impression_tag_iteminfo(&$items) {
 
     $items_id = array(); 
 
-    foreach (array_keys($items) as $cat_id) {
+    foreach ( array_keys( $items ) as $cat_id ) {
         // Some handling here to build the link upon catid 
             // If catid is not used, just skip it 
-        foreach (array_keys($items[$cat_id]) as $item_id) {
+        foreach (array_keys( $items[$cat_id] ) as $item_id ) {
             // In article, the item_id is "art_id" 
-            $items_id[] = intval($item_id); 
+            $items_id[] = intval( $item_id ); 
         } 
     } 
 
-    foreach (array_keys($items) as $cat_id) {
-        foreach (array_keys($items[$cat_id]) as $item_id) {
-            $sql = "SELECT l.aid, l.cid as lcid, l.title as ltitle, l.published, l.submitter, l.introtext, l.item_tag FROM " . $xoopsDB -> prefix('impression_articles') . " l, " . $xoopsDB -> prefix('impression_cat') . " c WHERE l.aid=" . $item_id . " AND l.cid=c.cid AND l.status=0 ORDER BY l.published DESC";
-            $result = $xoopsDB -> query($sql);
-            $row = $xoopsDB -> fetchArray($result);
+    foreach ( array_keys( $items ) as $cat_id ) {
+        foreach ( array_keys( $items[$cat_id] ) as $item_id ) {
+            $sql = "SELECT l.aid, l.cid as lcid, l.title as ltitle, l.published, l.submitter, l.introtext, l.item_tag FROM " . $xoopsDB -> prefix( 'impression_articles' ) . " l, " . $xoopsDB -> prefix( 'impression_cat' ) . " c WHERE l.aid=" . $item_id . " AND l.cid=c.cid AND l.status=0 ORDER BY l.published DESC";
+            $result = $xoopsDB -> query( $sql );
+            $row = $xoopsDB -> fetchArray( $result );
             $lcid = $row['lcid'];
             $items[$cat_id][$item_id] = array(
-                "title"      => '<img src="' . XOOPS_URL . '/modules/' . $mydirname . '/images/icon/impression.png" alt="" />&nbsp;' . $row['ltitle'],
+                "title"      => '<img src="' . ICMS_URL . '/modules/' . $mydirname . '/images/icon/impression.png" alt="" />&nbsp;' . $row['ltitle'],
                 "uid"        => $row['submitter'],
-                "link"       => "singlearticle.php?cid=$lcid&amp;aid=$item_id",
+                "link"       => 'singlearticle.php?cid=$lcid&amp;aid=$item_id',
                 "time"       => $row['published'],
                 "tags"       => $row['item_tag'],
                 "content"    => $row['introtext']
@@ -73,7 +71,7 @@ function impression_tag_iteminfo(&$items) {
 }
 
 /** Remove orphan tag-item links **/
-function impression_tag_synchronization($mid) {
+function impression_tag_synchronization( $mid ) {
   // Optional
 }
 ?>
