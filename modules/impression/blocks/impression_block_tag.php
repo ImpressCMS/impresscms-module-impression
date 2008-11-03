d0b091bbb7a1d5a193a6afe2b5ada2b51e569f44
Ronald <pietjebell31@hotmail.com>
2008-11-03 12:24:59 +0000

    Adding file headers
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk@6353 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/impression/blocks/impression_block_tag.php
+++ modules/impression/blocks/impression_block_tag.php
@@ -1,25 +1,43 @@
 <?php
+/**
+* Impression - a 'light' article management module for ImpressCMS
+*
+* Based upon WF-Links 1.06
+*
+* File: /blocks/impression_block_tag.php
+*
+* @copyright		http://www.xoops.org/ The XOOPS Project
+* @copyright		XOOPS_copyrights.txt
+* @copyright		http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* @package		Impression
+* @since			1.00
+* @author		McDonald
+* @version		$Id$
+*/
 
-function impression_tag_block_cloud_show($options) {
+function impression_tag_block_cloud_show( $options ) {
         $mydirname = basename( dirname(  dirname( __FILE__ ) ) );
-	include_once ICMS_ROOT_PATH."/modules/tag/blocks/block.php";
-	return tag_block_cloud_show($options, $mydirname);
+	include_once ICMS_ROOT_PATH . '/modules/tag/blocks/block.php';
+	return tag_block_cloud_show( $options, $mydirname );
 }
 
-function impression_tag_block_cloud_edit($options) {
-	include_once ICMS_ROOT_PATH."/modules/tag/blocks/block.php";
-	return tag_block_cloud_edit($options);
+function impression_tag_block_cloud_edit( $options ) {
+	include_once ICMS_ROOT_PATH . '/modules/tag/blocks/block.php';
+	return tag_block_cloud_edit( $options );
 }
 
-function impression_tag_block_top_show($options) {
+function impression_tag_block_top_show( $options ) {
         $mydirname = basename( dirname(  dirname( __FILE__ ) ) );
-	include_once ICMS_ROOT_PATH."/modules/tag/blocks/block.php";
-	return tag_block_top_show($options, $mydirname);
+	include_once ICMS_ROOT_PATH . '/modules/tag/blocks/block.php';
+	return tag_block_top_show( $options, $mydirname );
 }
 
-function impression_tag_block_top_edit($options) {
-        include_once ICMS_ROOT_PATH."/modules/tag/blocks/block.php";
-	return tag_block_top_edit($options);
+function impression_tag_block_top_edit( $options ) {
+        include_once ICMS_ROOT_PATH . '/modules/tag/blocks/block.php';
+	return tag_block_top_edit( $options );
 }
 
 ?>
