b10aefe64e029775343ccbff0ddbb1fbc1d92175
Ronald <pietjebell31@hotmail.com>
2008-11-03 15:09:06 +0000

    Applying file headers
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk@6366 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/impression/class/class_thumbnail.php
+++ modules/impression/class/class_thumbnail.php
@@ -1,8 +1,30 @@
 <?php
 /**
- * $Id: class_thumbnail.php
- * Module: Impression
- */
+* Impression - a 'light' article management module for ImpressCMS
+*
+* Based upon WF-Links 1.06
+*
+* File: /class/class_thumbnail.php
+*
+* @copyright		http://www.xoops.org/ The XOOPS Project
+* @copyright		XOOPS_copyrights.txt
+* @copyright		http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* @package		WF-Links 
+* @since			1.03
+* @author		John N
+* ----------------------------------------------------------------------------------------------------------
+* 				WF-Links 
+* @since			1.03b and 1.03c
+* @author		McDonald
+* ----------------------------------------------------------------------------------------------------------
+* 				Impression
+* @since			1.00
+* @author		McDonald
+* @version		$Id$
+*/
 
 if ( !defined( '_PATH' ) ) {
     define( "_PATH", ICMS_ROOT_PATH );
