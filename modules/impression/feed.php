3dc17dbe4f33ed9ed858524f938e124e32a95ba6
Ronald <pietjebell31@hotmail.com>
2008-11-09 03:03:37 +0000

    Added author and category to rss feed
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk@6596 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/impression/feed.php
+++ modules/impression/feed.php
@@ -27,9 +27,16 @@ $myFeed = new IcmsFeed();
 
 $myFeed -> webMaster = '';  // Admin contact email as stated in general preferences.
 	
-$sql = $xoopsDB -> query( 'SELECT aid, cid, title, published, introtext FROM ' . $xoopsDB -> prefix( 'impression_articles' ) . ' WHERE published > 0 AND published <= ' . time() . ' AND status=0 ORDER BY published DESC ', $xoopsModuleConfig['rssfeedtotal'], 0 );
+$sql = $xoopsDB -> query( 'SELECT aid, cid, title, published, introtext, publisher FROM ' . $xoopsDB -> prefix( 'impression_articles' ) . ' WHERE published > 0 AND published <= ' . time() . ' AND status=0 ORDER BY published DESC ', $xoopsModuleConfig['rssfeedtotal'], 0 );
     while ( $myrow = $xoopsDB -> fetchArray( $sql ) ) {	
 		
+		// First get the main category title of the link
+		$sql2 = $xoopsDB -> query( 'SELECT title FROM ' . $xoopsDB -> prefix('impression_cat') . ' WHERE cid=' . $myrow['cid'] );
+        $mycat = $xoopsDB -> fetchArray( $sql2 );
+		$category = htmlspecialchars( $mycat['title'] );
+		
+		// Get date, title, definition (shortened), author and the url of the link
+		$author= $myrow['publisher'];
 		$title = htmlspecialchars( $myrow['title'] );
 		$date  = formatTimestamp( $myrow['published'], 'D, d M Y H:i:s' );
 		$text  = htmlspecialchars( $impressionmyts -> displayTarea( $myrow['introtext'], 1, 1, 1, 1, 1 ) );
@@ -40,6 +47,8 @@ $sql = $xoopsDB -> query( 'SELECT aid, cid, title, published, introtext FROM ' .
 			'link' 			=> $link,
 			'description' 	=> $text,
 			'pubdate' 		=> $date,
+			'category'		=> $category,
+			'author'		=> $author,
 			'guid' 			=> $link
 			);
 	}
