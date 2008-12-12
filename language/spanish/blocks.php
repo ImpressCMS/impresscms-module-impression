88320c51fb55109aa1adc790d08dc73e15651934
Juan Calzado <debianus@impresscms.org>
2008-12-12 00:45:10 +0000

    Starting Spanish translation for Impression - finished at 60%
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk/modules/impression@7598 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/spanish/blocks.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * $Id: blocks.php
+ * Module: Impression
+ * Language: Spanish
+ * Format: UTF-8
+ * Author: McDonald
+ * Translator: debianus
+ */
+ 
+// Blocks
+define( '_MB_IMPRESSION_DISP', '<b>Mostrar: </b>' );
+define( '_MB_IMPRESSION_FILES', 'artículos' );
+define( '_MB_IMPRESSION_CHARS', '<b>Longitud del título: </b>' );
+define( '_MB_IMPRESSION_LENGTH', ' caracteres' );
+define( '_MB_IMPRESSION_EDIT', 'Modificar artículo' );
+define( '_MB_IMPRESSION_DELETE', 'Eliminar artículo' );
+define( '_MB_IMPRESSION_APPROVE', 'Aprobar' );
+define( '_MB_IMPRESSION_CATEGORY', 'Categoría' );
+define( '_MB_IMPRESSION_ARTICLE', 'Título del artículo' );
+define( '_MB_IMPRESSION_DATE', 'Fecha' );
+define( '_MB_IMPRESSION_HEADLINES', 'Titulares' );
+define( '_MB_IMPRESSION_ADMINSECTION', 'Sección de administración' );
+define( '_MB_IMPRESSION_DATEFORMAT', '<b>Formato de la fecha: </b>' );
+define( '_MB_IMPRESSION_DATEFORMATMANUAL', 'Lea el <a href="http://jp.php.net/manual/en/function.date.php" target="_blank">manual de PHP</a>' );
+define( '_MB_IMPRESSION_SELECTFEED', '<b>Seleccionar origen de RSS: </b>' );
+define( '_MB_IMPRESSION_SELECTFEED_DSC', '<small><br /> - Seleccione iCMS para usar el origen de ImpressCMS.<br /> - Seleccione RSSfit para el usar el módulo RSSfit de Brandycoke\'s RSSfit (los suborígenes necesitan ser activados).</small>'  );
+define( '_MB_IMPRESSION_READMORE', 'Leer más' );
+define( "_MB_IMPRESSION_PUBLISHEDON", "Publicado el " );
+define( "_MB_IMPRESSION_BY", " por " );
+define( "_MB_IMPRESSION_ARTICLEHITS", "(%s lecturas)" );
+define( '_MB_IMPRESSION_FEED', 'Origen de RSS' );
+define( '_MB_IMPRESSION_ICMSFEED', 'iCMS' );
+define( '_MB_IMPRESSION_RSSFITFEED', 'RSSfit' );
+?>
---
+++ language/spanish/main.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * $Id: main.php
+ * Module: Impression
+ * Language: Spanish
+ * Format: UTF-8
+ * Author: McDonald
+ * Translator: debianus
+ */
+
+define( "_MD_IMPRESSION_NOARTICLE", "El artículo no existe" );
+define( "_MD_IMPRESSION_SUBCATLISTING", "Lista de categorías" );
+define( "_MD_IMPRESSION_ISADMINNOTICE", "Administrador: hubo un problema con esta imagen." );
+define( "_MD_IMPRESSION_THANKSFORINFO", "Gracias por su envío." );
+define( "_MD_IMPRESSION_ISAPPROVED", "Gracias por su envío.<br />Su artículo ha sido aprobado y aparecerá en nuestra web." );
+define( "_MD_IMPRESSION_THANKSFORHELP", "Gracias por ayudarnos a mantener nuestra web en perfectas condiciones." );
+define( "_MD_IMPRESSION_FORSECURITY", "Por motivos de seguridad, su nombre de usuario y la dirección IP serán temporalmente guardadas." );
+define( "_MD_IMPRESSION_DESCRIPTION", "Descripción" );
+define( "_MD_IMPRESSION_SUBMITCATHEAD", "Formulario para el envío de un artículo" );
+define( "_MD_IMPRESSION_MAIN", "Inicio" );
+define( "_MD_IMPRESSION_POPULAR", "Popular" );
+define( "_MD_IMPRESSION_NEWTHISWEEK", "Nuevo esta semana" );
+define( "_MD_IMPRESSION_UPTHISWEEK", "Actualizado esta semana" );
+define( "_MD_IMPRESSION_POPULARITYLTOM", "lecturas (de menos a más)" );
+define( "_MD_IMPRESSION_POPULARITYMTOL", "lecturas (de más a menos)" );
+define( "_MD_IMPRESSION_TITLEATOZ", "título (de la A a la Z)" );
+define( "_MD_IMPRESSION_TITLEZTOA", "título (de la Z a la A)" );
+define( "_MD_IMPRESSION_DATEOLD", "fecha (los más antiguos primero)" );
+define( "_MD_IMPRESSION_DATENEW", "fecha (los más nuevos primero)" );
+define( "_MD_IMPRESSION_INTROTEXTC", "Texto de introducción: " );
+define( "_MD_IMPRESSION_INTROTEXTC_DSC", "<br />Este texto será la primera parte o cabecera del artículo y será mostrado en el bloque de titulares." );
+define( "_MD_IMPRESSION_DESCRIPTIONC", "Texto principal: " );
+define( "_MD_IMPRESSION_DESCRIPTIONC_DSC", "<br />Este texto, junto con el anterior, formarán el completo contenido del artículo" );
+define( "_MD_IMPRESSION_CATEGORYC", "Categoría: " );
+define( "_MD_IMPRESSION_VERSION", "Versión" );
+define( "_MD_IMPRESSION_SUBMITDATE", "Enviado el" );
+define( "_MD_IMPRESSION_ARTICLEHITS", "(%s lecturas)" );
+define( "_MD_IMPRESSION_PUBLISHERC", "Autor: " );
+define( "_MD_IMPRESSION_MODIFY", "Modificar" );
+define( "_MD_IMPRESSION_SUBMITBROKEN", "Enviar" );
+define( "_MD_IMPRESSION_TELLAFRIEND", "Recomendar" );
+define( "_MD_IMPRESSION_EDIT", "Modificar" );
+define( "_MD_IMPRESSION_THEREARE", "Hay <b>%s</b> <i>categoríass</i> y <b>%s</b> <i>artículos</i> listados" );
+define( "_MD_IMPRESSION_THEREIS", "Hay <b>%s</b> <i>categorías</i> y <b>%s</b> <i>artículos</i> listados" );
+define( "_MD_IMPRESSION_LATESTLIST", "Últimos artículos" );
+define( "_MD_IMPRESSION_FILETITLE", "Título del artículo: " );
+define( "_MD_IMPRESSION_ARTICLE_PUBLISHER", "Autor:" );
+define( "_MD_IMPRESSION_HOMEPAGEC", "Página web: " );
+define( "_MD_IMPRESSION_NOTSPECIFIED", "No especificado" );
+define( "_MD_IMPRESSION_PUBLISHER", "Autor" );
+define( "_MD_IMPRESSION_UPDATEDON", "Actualizado el" );
+define( "_MD_IMPRESSION_PRICEFREE", "Gratis" );
+define( "_MD_IMPRESSION_READMORE", "Leer más" );
+define( "_MD_IMPRESSION_OPTIONS", 'Opciones: ' );
+define( "_MD_IMPRESSION_NOTIFYAPPROVE", 'Notificarme cuando este artículo esté aprobado' );
+define( "_MD_IMPRESSION_INTFILEFOUND", "Hay un buen artículo para leer en %s" ); // %s is your site name
+define( "_MD_IMPRESSION_CATEGORY", "Categoría" );
+define( "_MD_IMPRESSION_HITS", "Lecturas" );
+define( "_MD_IMPRESSION_SORTBY", "Ordenar por:" );
+define( "_MD_IMPRESSION_TITLE", "título" );
+define( "_MD_IMPRESSION_DATE", "fecha" );
+define( "_MD_IMPRESSION_POPULARITY", "lecturas" );
+define( "_MD_IMPRESSION_CURSORTBY", "Artículos actualmente ordenados por: %s" );
+define( "_MD_IMPRESSION_CANCEL", "Cancelar" );
+define( "_MD_IMPRESSION_MUSTREGFIRST", "Lo sentimos: no tiene permiso para realizar esta acción.<br />Por favor, regístrese en el sitio o acceda como usuario registrado" );
+define( "_MD_IMPRESSION_SUBMITARTICLE", "Enviar un artículo" );
+define( "_MD_IMPRESSION_SUB_SNEWMNAMEDESC", "<ul><li>Todos los nuevos artículos están sujetos a aprobación previa a su publicación y puede la misma tardar al menos un día, de modo que entretanto no aparecerá en la web.</li><li>Nos reservamos el derecho de rehusar la publicación de cualquier artículo o cambiar su contenido para su aprobación y publicación.</li></ul>" );
+define( "_MD_IMPRESSION_MAINLISTING", "Categoría principal" );//Main category listings
+define( "_MD_IMPRESSION_LASTWEEK", "Última semana" );
+define( "_MD_IMPRESSION_LAST30DAYS", "Últimos 30 días" );
+define( "_MD_IMPRESSION_1WEEK", "1 semana" );
+define( "_MD_IMPRESSION_2WEEKS", "2 semanas" );
+define( "_MD_IMPRESSION_30DAYS", "30 días" );
+define( "_MD_IMPRESSION_SHOW", "Mostrar" );
+define( "_MD_IMPRESSION_DAYS", "días" );
+define( "_MD_IMPRESSION_NEWlinks", "Nuevos artículos" );
+define( "_MD_IMPRESSION_TOTALNEWARTICLES", "Total de nuevos artículos" );
+define( "_MD_IMPRESSION_DTOTALFORLAST", "Total de últimos nuevos artículos" );
+define( "_MD_IMPRESSION_AGREE", "De acuerdo" );
+define( "_MD_IMPRESSION_DOYOUAGREE", "¿Está de acuerdo con las anteriores condiciones?" );
+define( "_MD_IMPRESSION_DISCLAIMERAGREEMENT", "Limitación de responsabilidad" );
+define( "_MD_IMPRESSION_RESOURCEID", "Id. del recurso: " );
+define( "_MD_IMPRESSION_REPORTER", "Autor del envío: " );
+define( "_MD_IMPRESSION_DATEREPORTED", "Fecha del envío: " );
+define( "_MD_IMPRESSION_BROWSETOTOPIC", "<b>Examinar artículos por orden alfabético</b>" );
+
+define( "_MD_IMPRESSION_DELETE", "Eliminar" );
+define( "_MD_IMPRESSION_DISPLAYING", "Mostrar por: " );
+define( "_MD_IMPRESSION_LEGENDTEXTNEW", "Nuevo hoy" );
+define( "_MD_IMPRESSION_LEGENDTEXTNEWTHREE", "Nuevos en los tres últimos días" );
+define( "_MD_IMPRESSION_LEGENDTEXTTHISWEEK", "Nuevos esta semana" );
+define( "_MD_IMPRESSION_LEGENDTEXTNEWLAST", "De más de una semana de antiguedad" );
+define( "_MD_IMPRESSION_THISFILEDOESNOTEXIST", "Error: el artículo no existe" );
+define( "_MD_IMPRESSION_BROKENREPORTED", "Reporte de artículo roto" );
+
+define( "_MD_IMPRESSION_REV_SNEWMNAMEDESC", "Por favor, complete el formulario que se muestra e intentaremos revisar su envío tan pronto como nos sea posible.<br /><br />Gracias por facilitarnos su opinión.<br /><br />Todas las reseñas serán revisadas por alguno de nuestros administradores antes de su publicación en el sitio." );
+define( "_MD_IMPRESSION_ISNOTAPPROVED", "Su envío debe ser aprobado previamente a su publicación por un moderador." );
+define( "_MD_IMPRESSION_HOMEPAGETITLEC", "Título de la página de inicio: " );
+define( "_MD_IMPRESSION_SCREENSHOTCLICK", "Display full image" );
+define( "_MD_IMPRESSION_OTHERBYUID", "Otros artículos por: " );
+define( "_MD_IMPRESSION_NOOTHERBYUID", "No hay más artículos por: " );
+define( "_MD_IMPRESSION_LINKTIMES", "Article times: " );
+define( "_MD_IMPRESSION_MAINTOTAL", "Total de artículos: " );
+define( "_MD_IMPRESSION_ARTICLENOW", "Ver artículo" );
+define( "_MD_IMPRESSION_PAGES", "<b>Páginas</b>" );
+define( "_MD_IMPRESSION_SUBMITTER", "Autor" );
+define( "_MD_IMPRESSION_ERROR", "Error al actualizar la base de datos: la información no fue guardada." );
+define( "_MD_IMPRESSION_COPYRIGHT", "Copyright" );
+
+// added frankblack
+define( "_MD_IMPRESSION_NOTALLOWESTOSUBMIT", "No tiene permiso para enviar artículos" );
+define( "_MD_IMPRESSION_INFONOSAVEDB", "La información no se guardó en la base de datos: <br /><br />" );
+
+define( "_MD_IMPRESSION_NEWLAST", "Publicados antes de la semana anterior" );
+define( "_MD_IMPRESSION_NEWTHIS", "Publicados en esta semana" );
+define( "_MD_IMPRESSION_THREE", "Publicados en los últimos tres días" );
+define( "_MD_IMPRESSION_TODAY", "Publicado hoy" );
+define( "_MD_IMPRESSION_NO_FILES", "No hay artículos todavía" );
+
+define( "_MD_IMPRESSION_NOPERMISSIONTOPOST", "No tiene permiso para enviar un artículo en esta categoría." );
+define( "_MD_IMPRESSION_RESOURCES", "Recursos" );
+
+define( "_MD_IMPRESSION_PUBLISHDATE", "Publicado" );
+define( "_MD_IMPRESSION_APPROVE", "Aprobar" );
+define( "_MD_IMPRESSION_MODERATOR_OPTIONS", "Opciones de moderación" );
+
+// added by McDonald
+define( "_MD_IMPRESSION_KEYWORDS", "Palabras clave:" );
+define( "_MD_IMPRESSION_KEYWORDS_NOTE", "Deben estar separadas con una coma<br />(<i>palabra1, palabra2, etc.</i>)" );
+define( "_MD_IMPRESSION_NOARTICLELOAD", "Gracias por su envío" );
+define( "_MD_IMPRESSION_LINKID", "Id. del artículo" );
+define( "_MD_IMPRESSION_ADDTO", "Añadir a: " );
+define( "_MD_IMPRESSION_NEWARTICLES", "Últimos artículos" );
+define( "_MD_IMPRESSION_TAKINGUBACK", "Volver." );
+define( "_MD_IMPRESSION_PRINT", "Imprimir el artículo" );
+define( "_MD_IMPRESSION_PDF", "PDF" );
+define( "_MD_IMPRESSION_PUBLISHEDON", "Publicado el " );
+define( "_MD_IMPRESSION_BY", " por " );
+define( "_MD_IMPRESSION_NOITEMSELECTED", "No ha seleccionado un artículo válido");
+define( "_MD_IMPRESSION_ARTICLESCAT", " artículos" );
+define( "_MD_IMPRESSION_ADMINSECTION", "Sección de administración" );
+define( "_MD_IMPRESSION_ARTICLE", "Título del artículo" );
+define( "_MD_IMPRESSION_WHO_WHEN", "Publicado por %s el %s" );
+define( "_MD_IMPRESSION_RANK", "Rango" );
+//define( "_MD_IMPRESSION_EDIT", "Edit artículo" );
+//define( "_MD_IMPRESSION_DELETE", "Delete article" );
+define( '_MD_IMPRESSION_FEED', 'Fuente de RSS' );
+define( '_MD_IMPRESSION_TEXTOPTIONS', 'Opciones del texto:' );
+define( '_MD_IMPRESSION_DISABLEBREAK', '¿Usar conversión del salto de línea de iCMS?' );
+define( '_MD_IMPRESSION_BACKBUTTON', 'Volver' );
+?>
