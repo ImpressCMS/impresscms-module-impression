c3785d1869e42b81819870f53e52619e4e13cee5
Ronald <pietjebell31@hotmail.com>
2012-02-27 15:38:47 +0000

    Updated, no more 'errors'
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk@23752 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/impression/include/update.php
+++ modules/impression/include/update.php
@@ -28,91 +28,58 @@
  
 if ( !defined( 'ICMS_ROOT_PATH' ) ) { die( 'ICMS root path not defined' ); }
 
-// referer check
+// Referer check
 $ref = xoops_getenv( 'HTTP_REFERER' );
 
 if( $ref == '' || strpos( $ref , ICMS_URL . '/modules/system/admin.php' ) === 0 ) {
-	$i=0;
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " ADD COLUMN inblocks TINYINT(1) NOT NULL default '1'" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " ADD COLUMN inblocks TINYINT(1) NOT NULL default '1'";
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " DROP nohtml" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " DROP nohtml";
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " DROP nosmiley" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " DROP nosmiley" );
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " DROP noxcodes" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " DROP noxcodes" );
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " DROP noimages" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " DROP noimages" );
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " DROP nobreak" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " DROP nobreak" );
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " MODIFY title VARCHAR(255) NOT NULL default ''" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_cat') . " MODIFY title VARCHAR(255) NOT NULL default ''" );
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_indexpage') . " DROP nohtml" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_indexpage') . " DROP nohtml" );
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_indexpage') . " DROP nosmiley" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_indexpage') . " DROP nosmiley" );
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_indexpage') . " DROP noxcodes" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_indexpage') . " DROP noxcodes" );
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_indexpage') . " DROP noimages" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_indexpage') . " DROP noimages" );
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_indexpage') . " DROP nobreak" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_indexpage') . " DROP nobreak" );
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_articles') . " DROP nobreak" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_articles') . " DROP nobreak" );
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_mod') . " DROP nobreak" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_mod') . " DROP nobreak" );
+		if ( !mysql_query( $sql ) ) {}
 
-		// Version
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_articles') . " ADD COLUMN source VARCHAR(255) NOT NULL default ''" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		// Version 1.11
+		$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_articles') . " ADD COLUMN source VARCHAR(255) NOT NULL default ''";
+		if ( !mysql_query( $sql ) ) {}
 
-		$i++;
-		$ret[$i] = true;
-		$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_articles') . " ADD COLUMN sourceurl VARCHAR(255) NOT NULL default ''" );
-		$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+		$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'impression_articles') . " ADD COLUMN sourceurl VARCHAR(255) NOT NULL default ''";
+		if ( !mysql_query( $sql ) ) {}
 }
 ?>
