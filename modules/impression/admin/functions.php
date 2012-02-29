24c7d16b6af9e21854980584dda7c5fdb1835d4b
Ronald <pietjebell31@hotmail.com>
2012-02-29 12:17:01 +0000

    Articles can be set online/offline by clicking icon
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/impression/trunk@23792 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- modules/impression/admin/functions.php
+++ modules/impression/admin/functions.php
@@ -143,14 +143,15 @@ function impression_articlelistbody( $published ) {
 	$publish = ( $published['published'] > 0 ) ? impression_time( formatTimestamp( $published['published'], icms::$module -> config['dateformatadmin'] ) ) : _AM_IMPRESSION_NOTPUBLiSHED;
 
 	if ( $published['status'] == 0 && ( $published['published'] && $published['published'] < time() ) ) {
-		$published_status = $imagearray['online'];
+		$published_status = '<a href="index.php?op=status_off&amp;aid=' . $aid . '">' . $imagearray['online'] . '</a>';;
 	} elseif ( $published['status'] == 2 ) {
 		$published_status = $imagearray['rejected'];
 	} elseif ( $published['status'] == 3 ) {
 		$published_status = $imagearray['submitted'];
 	} else {
-		$published_status = $imagearray['offline'];
+		$published_status = ( $published['published'] == 0 ) ? "<a href='newarticles.php'>" . $imagearray['offline'] . "</a>" : '<a href="index.php?op=status_on&amp;aid=' . $aid . '">' . $imagearray['offline'] . '</a>';
 	}
+
 	$icon  = '<a href="index.php?op=edit&amp;aid=' . $aid . '" title="' . _AM_IMPRESSION_ICO_EDIT . '">' . $imagearray['editimg'] . '</a>';
 	$icon .= '<a style="padding-left: 5px;" href="index.php?op=delete&amp;aid=' . $aid . '" title="' . _AM_IMPRESSION_ICO_DELETE . '">' . $imagearray['deleteimg'] . '</a>';
 	$icon .= '<a style="padding-left: 5px;" href="index.php?op=clone&amp;aid=' . $aid . '" title="' . _AM_IMPRESSION_ICO_CLONE . '">' . $imagearray['clone'] . '</a>';
